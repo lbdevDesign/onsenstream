5547bb492baaaa418c489882d2c28f985d6a4721
Lucas <lucasbombaron@gmail.com>
2024-04-09 21:52:58 +0200

    Initialisation

---
+++ src/pages/homepage/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+//Organisms
+import Header from "../../components/organisms/header/header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;

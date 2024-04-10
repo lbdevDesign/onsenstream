8931e77bdc1aa65a1c8dcd05a5c28148a4720484
Lucas <lucasbombaron@gmail.com>
2024-04-10 13:54:10 +0200

    data

---
+++ src/data/heroCarouselItems.js
@@ -0,0 +1,25 @@
+const heroCarouselData = [
+    {
+        id: "1",
+        img: "/assets/moviesCovers/Nightcall.jpg",
+        title :"Nightcall",
+        alt: "Nightcall",
+        description: "Oiseau de nuit asocial, Lou fait une halte sur le site d'un accident de la route. Fasciné par le travail des caméramans pigistes guidés sur les lieux par les radars de la police, le jeune voleur décide de faire de ce métier sa nouvelle vocation."
+    },
+    {
+        id: "2",
+        img: "/assets/moviesCovers/Gladiator.jpg",
+        title :"Gladiator",
+        alt: "Gladiator",
+        description: "Le fils de l'empereur romain Marc Aurèle s'arroge brutalement le pouvoir et ordonne l'exécution du général Maximus, le plus fidèle soutien de son père. Maximus échappe à ses assassins mais tombe aux mains d'un marchand d'esclaves. Ses dons et son courage en font bientôt l'idole des arènes..."
+    },
+    {
+        id: "3",
+        img: "/assets/moviesCovers/justMercy.jpg",
+        title :"Just Mercy",
+        alt: "Just Mercy",
+        description: "Inspiré d'une histoire vraie, le parcours du jeune avocat Bryan Stevenson et son combat pour défendre des hommes jugés coupables à tort ou d'autres qui n'ont pas les moyens d'être défendus, avec le soutien de la jeune avocate Eva Ansley. Inspiré d'une histoire vraie dans les couloirs de la mort d'Alabama."
+    }
+];
+
+export default heroCarouselData;
---
+++ src/data/topMovies.js
@@ -0,0 +1,84 @@
+const topMoviesData = [
+    {
+        id: "1",
+        title: "Tár",
+        img: "/assets/moviesCovers/tar.jpg",
+        duration: "2h38",
+        likes: "3k8",
+        plateform: "/assets/streamingIcon/canal.png"
+    },
+    {
+        id: "2",
+        title: "Spider-man: Across the spider-verse",
+        img: "/assets/moviesCovers/smSpiderverse.jpeg",
+        duration: "2h21",
+        likes: "3k4",
+        plateform: "/assets/streamingIcon/canal.png"
+    },
+    {
+        id: "3",
+        title: "Oppenheimer",
+        img: "/assets/moviesCovers/oppenheimer.png",
+        duration: "3h01",
+        likes: "3k3",
+        plateform: "/assets/streamingIcon/netflix.png"
+    },
+    {
+        id: "4",
+        title: "Tár",
+        img: "/assets/moviesCovers/tar.jpg",
+        duration: "2h38",
+        likes: "3k8",
+        plateform: "/assets/streamingIcon/canal.png"
+    },
+    {
+        id: "5",
+        title: "Spider-man: Across the spider-verse",
+        img: "/assets/moviesCovers/smSpiderverse.jpeg",
+        duration: "2h21",
+        likes: "3k4",
+        plateform: "/assets/streamingIcon/canal.png"
+    },
+    {
+        id: "6",
+        title: "Oppenheimer",
+        img: "/assets/moviesCovers/oppenheimer.png",
+        duration: "3h01",
+        likes: "3k3",
+        plateform: "/assets/streamingIcon/netflix.png"
+    },
+    {
+        id: "7",
+        title: "Tár",
+        img: "/assets/moviesCovers/tar.jpg",
+        duration: "2h38",
+        likes: "3k8",
+        plateform: "/assets/streamingIcon/prime.png"
+    },
+    {
+        id: "8",
+        title: "Spider-man: Across the spider-verse",
+        img: "/assets/moviesCovers/smSpiderverse.jpeg",
+        duration: "2h21",
+        likes: "3k4",
+        plateform: "/assets/streamingIcon/canal.png"
+    },
+    {
+        id: "9",
+        title: "Oppenheimer",
+        img: "/assets/moviesCovers/oppenheimer.png",
+        duration: "3h01",
+        likes: "3k3",
+        plateform: "/assets/streamingIcon/netflix.png"
+    },
+    {
+        id: "10",
+        title: "Past Lives",
+        img: "/assets/moviesCovers/pastLives.jpeg",
+        duration: "1h46",
+        likes: "2k3",
+        plateform: "/assets/streamingIcon/prime.png"
+    }
+];
+
+export default topMoviesData;

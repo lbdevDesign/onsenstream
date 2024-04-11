d838d9eedc8459381ac058f3002515fae57ab12b
Lucas <lucasbombaron@gmail.com>
2024-04-11 10:20:56 +0200

    homepage and scss update

--- src/pages/homepage/App.jsx
+++ src/pages/homepage/App.jsx
@@ -3,7 +3,11 @@ import React from "react";
 //Organisms
 import Header from "../../components/organisms/header/header";
 import { HeroCarousel } from "../../components/organisms/heroCarousel/heroCarousel";
+import MovieCarousel from "../../components/organisms/movieSlider/movieSlider";
 
+//Data
+import topMoviesData from '../../data/topMovies';
+import newMoviesData from '../../data/newMovies';
 
 
 function App() {
@@ -15,6 +19,10 @@ function App() {
       </div>
       <div className="App__hero">
         <HeroCarousel />
+      </div>
+      <div className="App__movies">
+        <MovieCarousel title='Les mieux notés' movies={topMoviesData || []}/>
+        <MovieCarousel title='Nouveautés' movies={newMoviesData || []} />
       </div>
     </div>
   );

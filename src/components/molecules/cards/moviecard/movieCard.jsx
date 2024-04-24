1eccda82bccec71e2d81d50a29291466264b1819
Lucas <lucasbombaron@gmail.com>
2024-04-24 12:57:49 +0200

    moviecard update

--- src/components/molecules/cards/moviecard/movieCard.jsx
+++ src/components/molecules/cards/moviecard/movieCard.jsx
@@ -8,27 +8,18 @@ import { ClockIcon } from '@heroicons/react/24/solid';
 import { HandThumbUpIcon } from '@heroicons/react/24/solid';
 
 function MovieCard({ ...item }) {
+
+    const coverPath = `https://image.tmdb.org/t/p/w500${item.poster_path}`
+
     return(
         <a className="moviecard">
-            <img src={item.img} className="moviecard__img" alt={item.title}/>
+            <img src={coverPath} className="moviecard__img" alt={item.title}/>
             <p className="moviecard__title">{item.title}</p>
             <div className="moviecard__infos">
-                <CardTag text={item.duration}>
-                    <ClockIcon className="moviecard__infos__icon" alt="Durée"/>
-                </CardTag>
-                {/* <div className="moviecard__infos__info">
-                    <ClockIcon className="moviecard__icon" alt="Durée"/>
-                    <p className="moviecard__text">{item.duration}</p>
-                </div> */}
                 <CardTag text={item.likes}>
-                    <img src={item.plateform} className="moviecard__infos__streaming" alt="Likes"/>
                     <HandThumbUpIcon className="moviecard__infos__icon" alt="Likes"/>
+                    <p>{item.vote_average} / 10</p>
                 </CardTag>
-                {/* <div className="moviecard__infos__info">
-                    <img src={item.plateform} className="moviecard__streaming" alt="Likes"/>
-                    <HandThumbUpIcon className="moviecard__icon" alt="Likes"/>
-                    <p className="moviecard__text">{item.likes}</p>
-                </div> */}
             </div>
         </a>
     )

89e061370c4bf54d4e91df264dc8d1c6c35e875a
Lucas <lucasbombaron@gmail.com>
2024-04-24 12:39:27 +0200

    genre card api

--- src/components/molecules/cards/genrecard/genreCard.jsx
+++ src/components/molecules/cards/genrecard/genreCard.jsx
@@ -1,23 +1,9 @@
 import React from "react";
 
-function GenreCard({ genre, movies }) {
-
-    const showCovers = movies.slice(0,4);
-    let showCoversUrl = [];
-    for (const item of showCovers) {
-        showCoversUrl.push(item.img);
-    }
-
-
+function GenreCard({ genre }) {
 
     return(
         <a className="genrecard">
-            <div className="genrecard__imggrid">
-                {showCoversUrl.map((item, index) => (
-                    <img key={index} src={item} alt="films" className="genrecard__imggrid__item"/>
-                ))}
-            </div>
-            <div className="genrecard__overlay"></div>
             <p className="genrecard__title">{genre}</p>
         </a>
     )

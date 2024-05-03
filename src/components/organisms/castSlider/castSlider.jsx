c75a079e7de2a11874e0152719c631a22bb9aea4
Lucas <lucasbombaron@gmail.com>
2024-05-03 15:01:33 +0200

    cardSlider update (ul)

---
+++ src/components/organisms/castSlider/castSlider.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+//Molecules
+import CastCard from "../../molecules/cards/castCard/castCard";
+
+//Atoms
+// import FuncButton from "../../atoms/buttons/functionalbutton/funcbutton";
+
+function CastSlider({ title, casting }) {
+
+    return (
+        <div className="castslider">
+          <div className="castslider__header">
+            <h2 className="castslider__header__title">{title}</h2>
+            {/* {isDesktop && (
+              <div className="castslider__header__nav">
+                <FuncButton variant="sm" onClick={()=>sliderRef.current.slickPrev()}>
+                    <ArrowLeftIcon className="icons" alt="précédent"/>
+                </FuncButton>
+                <FuncButton variant="sm" onClick={()=>sliderRef.current.slickNext()}>
+                    <ArrowRightIcon className="icons" alt="précédent"/>
+                </FuncButton>
+              </div>
+            )} */}
+          </div>
+    
+          <div className="castslider__box">
+            <div className="castslider__box__fade"></div>
+            <ul className="castslider__box__inner">
+              {casting.map((item) => (
+                <li>
+                    <CastCard key={item.id} {...item}/>
+                </li>
+              ))}
+            </ul>
+    
+          </div>
+            
+        </div>
+      )
+    }
+    
+    export default CastSlider;

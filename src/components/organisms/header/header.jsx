973463ad0c65507df24612330e1fe595e35c0a0f
Lucas <lucasbombaron@gmail.com>
2024-05-06 15:48:29 +0200

    header update

--- src/components/organisms/header/header.jsx
+++ src/components/organisms/header/header.jsx
@@ -1,25 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-//Atoms
-// import FuncButton from '../../atoms/buttons/functionalbutton/funcbutton';
+//Data
+import useMovieListData from "../../../data/movieList";
+import useSerieListData from "../../../data/serieList";
+
+//Organisms
+import SearchResults from '../searchresults/searchResults';
 
 //Molecules
-// import { Searchbar } from '../../molecules/searchbar/searchbar';
+import { Searchbar } from '../../molecules/searchbar/searchbar';
+
+//Atoms
+import FuncButton from "../../atoms/buttons/functionalbutton/funcbutton"
+import CtaButton from '../../atoms/buttons/ctabutton/ctabutton';
 
 //Assets
 import osesLogo from '../../../assets/OSSLogo.svg';
-// import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
-// import {XMarkIcon} from '@heroicons/react/24/outline';
+import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
+import {XMarkIcon} from '@heroicons/react/24/outline';
+
 // import {UserIcon} from '@heroicons/react/24/outline';
-import { Link } from 'react-router-dom';
 
 function Header() {
 
-    // const [showSearchbar, setSearchbar] = useState();
+    const [showSearchbar, setSearchbar] = useState(false);
+    const [showSerchField, setShowSearchField] = useState(false)
+    const [searchTerm, setSearchTerm] = useState("");
 
-    // function display() {
-    //     setSearchbar(value=>!value)
-    // }
+    const movieData = useMovieListData(searchTerm);
+    const serieData = useSerieListData(searchTerm);
+
+    const [movieResults, setMovieResults] = useState([]);
+    const [serieResults, setSerieResults] = useState([]);
+
+    useEffect(() => {
+        setMovieResults(movieData);
+        setSerieResults(serieData);
+    }, [movieData, serieData])
+    
+    function handleDisplay() {
+        setSearchbar(true)
+    }
+
+    function handleClose() {
+        setSearchbar(false)
+        setShowSearchField(false);
+    }
+
+    const handleInputChange = (event) => {
+        const searchInput = event.target.value;
+        setShowSearchField(searchTerm.length >= 2);
+        setSearchTerm(searchInput)
+    };
+
+    const movieResultsFiltered = movieResults.filter((item) => item.poster_path !== null && item.poster_path !== '');
+    const serieResultsFiltered = serieResults.filter((item) => item.poster_path !== null && item.poster_path !== '');
 
     // function handleAccountClick() {
     //     console.log("J'accède à mon compte");
@@ -27,35 +63,63 @@ function Header() {
 
     return(
         <div className="header">
-            <Link to={"/"}>
-                <img src={osesLogo} className='header__logo' alt="Logo On s'en stream" />
-            </Link>
-            {/* <div className='header__cta'>
-                {showSearchbar ? (
-                    <button className='header__cta__close' onClick={display}>
-                        <XMarkIcon className='header__cta__close--icon' alt='Close' />
-                    </button>
-                ) : (
-                    <>
-                        <button className='header__cta__search' onClick={display}>
-                            <MagnifyingGlassIcon className="icons" alt='Search' />
-                        </button>
-                        <div className='header__cta__searchbar'>
-                            <Searchbar placeholder='Film, genre, acteur...'/>
+
+            <div className="header__top">
+                <Link to={"/"}>
+                    <img src={osesLogo} className='header__top__logo' alt="Logo On s'en stream" />
+                </Link>
+                <div className='header__top__cta'>
+                    {showSearchbar ? (
+                        <div className='header__top__cta__mobile'>
+                            <FuncButton onClick={handleClose}>
+                                <XMarkIcon className='header__top__cta__mobile__close--icon' alt='Close' />
+                            </FuncButton>
                         </div>
-                        <FuncButton onClick={handleAccountClick}>
-                            <UserIcon className="icons" alt='Mon compte' />
-                        </FuncButton>
-                    </>
-                )}    
+                    ) : (
+                        <>
+                            <div className='header__top__cta__search'>
+                                <CtaButton variant="square"  onClick={handleDisplay}>
+                                    <MagnifyingGlassIcon className="icons" alt='Search' />
+                                </CtaButton>
+                            </div>
+                            <div className='header__top__cta__searchbar'>
+                                <Searchbar 
+                                    placeholder='Film, genre, acteur...'
+                                    onChange={handleInputChange}
+                                    onClick={handleClose}
+                                    icon={showSerchField ? "cross" : "glass"}
+                                />
+                            </div>
+                            {/* <FuncButton onClick={handleAccountClick}>
+                                <UserIcon className="icons" alt='Mon compte' />
+                            </FuncButton> */}
+                        </>
+                    )}    
+                </div>
             </div>
-            <div className={`header__searchfield${showSearchbar?' active':null}`}>
-                <div className='header__searchfield__bar'>
-                    <Searchbar placeholder='Film, genre, acteur...'/>
+
+            {showSearchbar ? (
+                <div className='header__top__cta__mobile__searchbar'>
+                    <Searchbar 
+                        placeholder='Film, genre, acteur...'
+                        onChange={handleInputChange}
+                        onClick={handleClose}
+                        icon={showSerchField ? "cross" : "glass"}
+                    />
                 </div>
-                <div className='header__searchfield__results'>
-                </div>
-            </div> */}
+            ) : (
+                <></>
+            )}
+            <div className={`header__searchfield${showSerchField?' active':null}`}>
+                {((movieResults.length === 0) && (serieResults.length === 0)) ? (
+                    <h2>Aucun Résultas</h2>
+                ) : (
+                    <div className='header__searchfield__results'>
+                        <SearchResults movies={movieResultsFiltered} series={serieResultsFiltered} />
+                    </div>
+                )}
+            </div> 
+
         </div>
     )
 }
